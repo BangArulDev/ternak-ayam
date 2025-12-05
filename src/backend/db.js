c7c82bc34b36538b58ce5bded50a23c0ffd29f4c
BangArulDev <95467162+BangArulDev@users.noreply.github.com>
2025-12-05 17:15:57 +0700

    Delete src directory

--- src/backend/db.js
+++
@@ -1,9 +0,0 @@
-const { createClient } = require("@supabase/supabase-js");
-
-const supabaseUrl = "https://vkvqnfdrvbbilbbzkxpk.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZrdnFuZmRydmJiaWxiYnpreHBrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ3MDMyMDQsImV4cCI6MjA4MDI3OTIwNH0.PEwI5qAE5fPOw4ohDxVzUBrxwTpFIkH9fgyBVdvfuwY";
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-module.exports = supabase;
